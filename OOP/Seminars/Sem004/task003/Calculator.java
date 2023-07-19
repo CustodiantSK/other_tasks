5cad94468ba45aa06bac85306fdae19baa5b1098
CustodiantSK <kripkovsky@yandex.ru>
2023-07-20 00:53:31 +0700

    Решение ДЗ

--- OOP/Seminars/Sem004/task003/Calculator.java
+++ OOP/Seminars/Sem004/task003/Calculator.java
@@ -1,5 +1,6 @@
 package Seminars.Sem004.task003;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
@@ -10,11 +11,73 @@ public class Calculator {
      * @param numbers массив чисел
      * @return Сумма всех элементов массива
      */
+
+    public List<Double> binaryString(List<String> numbers) {
+        List<Double> res = new ArrayList<>();
+        for (String number : numbers) {
+            res.add(Double.parseDouble(number));
+        }
+        return res;
+    }
+
     public Double sum(List<? extends Number> numbers) {
         Double res = 0.0;
         for (Number number : numbers) {
             res += number.doubleValue();
         }
         return res;
+    }
+
+    public Double subtr(List<? extends Number> numbers) {
+        Double res = 0.0;
+        int count = 0;
+        Double res2 = 0.0;
+        for (Number number : numbers) {
+            if (count == 0) {
+                res = number.doubleValue() - res2;
+            } else {
+                res -= number.doubleValue();
+            }
+            count++;
+            // System.out.println(res);
+        }
+        return res;
+    }
+
+    public Double mult(List<? extends Number> numbers) {
+        Double res = 1.0;
+        for (Number number : numbers) {
+            res *= number.doubleValue();
+        }
+        return res;
+    }
+
+    public Double div(List<? extends Number> numbers) {
+        Double res3 = 1.0;
+        int count = 0;
+        Double res4 = 1.0;
+        for (Number number : numbers) {
+            if (count == 0) {
+                res4 = number.doubleValue() / res3;
+            } else {
+                res4 /= number.doubleValue();
+            }
+            count++;
+            // System.out.println(res4);
+        }
+        return res4;
+    }
+
+    public void test(Object value) {
+
+        if (value instanceof Integer) {
+            System.out.println("Это Integer");
+        } else if (value instanceof String) {
+            System.out.println("Это String");
+        } else if (value instanceof Float) {
+            System.out.println("Это Float");
+        } else if (value instanceof Double) {
+            System.out.println("Это Double");
+        }
     }
 }
--- OOP/Seminars/Sem004/task003/Main.java
+++ OOP/Seminars/Sem004/task003/Main.java
@@ -6,13 +6,69 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
+
         List<Double> doubleList = new ArrayList<>();
         List<Integer> integerList = new ArrayList<>();
-        for (int i = 0; i <= 100; i += 50) {
+        List<String> stringList = new ArrayList<>();
+
+        for (int i = 10; i > 0; i -= 2) {
             doubleList.add((double) i);
             integerList.add(i);
+            String str = String.valueOf(i);
+            stringList.add(str);
         }
-        System.out.println("Sum double = " + calc.sum(doubleList));
-        System.out.println("Sum integer = " + calc.sum(integerList));
+
+        System.out.println("Проверим значения в листе doubleList " + doubleList);
+        for (int index = 0; index < doubleList.size(); index++) {
+            calc.test(doubleList.get(index));
+        }
+
+        System.out.println("Проверим значения в листе  integerList " + integerList);
+        for (int index = 0; index < integerList.size(); index++) {
+            calc.test(integerList.get(index));
+        }
+
+        System.out.println("Проверим значения в листе stringList " + stringList);
+        for (int index = 0; index < stringList.size(); index++) {
+            calc.test(stringList.get(index));
+        }
+
+        /**
+         * конвертация строк в формат дабл
+         */
+        List<Double> convertStringList = calc.binaryString(stringList);
+        System.out.println("Проверим значения в листе stringList" + convertStringList + " после конвертации");
+        for (int index = 0; index < convertStringList.size(); index++) {
+            calc.test(convertStringList.get(index));
+        }
+
+        /**
+         * сложение
+         */
+        System.out.println("Сложение");
+        System.out.println("Sum Double = " + calc.sum(doubleList));
+        System.out.println("Sum Integer = " + calc.sum(integerList));
+        System.out.println("Sum String = " + calc.sum(convertStringList));
+        /**
+         * Вычитание
+         */
+        System.out.println("Вычитание");
+        System.out.println("Subtr Double = " + calc.subtr(doubleList));
+        System.out.println("Subtr Integer = " + calc.subtr(integerList));
+        System.out.println("Subtr String = " + calc.subtr(convertStringList));
+        /**
+         * Умножение
+         */
+        System.out.println("Умножение");
+        System.out.println("Multiplikation double = " + calc.mult(doubleList));
+        System.out.println("Multiplikation Integer = " + calc.mult(integerList));
+        System.out.println("Multiplikation String = " + calc.mult(convertStringList));
+        /**
+         * деление
+         */
+        System.out.println("Деление");
+        System.out.println("Division double = " + calc.div(doubleList));
+        System.out.println("Division Integer = " + calc.div(integerList));
+        System.out.println("Division String = " + calc.div(convertStringList));
     }
 }
